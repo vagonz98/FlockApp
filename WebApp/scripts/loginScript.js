79d09aee93189c25f2bf1c890b7de3d3f5ef67d8
Kevin24816 <li.qingkai@yahoo.com>
2016-10-15 23:09:02 -0500

    blah

---
+++ WebApp/scripts/loginScript.js
@@ -0,0 +1,18 @@
+var http = new XMLHttpRequest();
+var url = "riceflock.com/signin";
+
+var username = document.getElementById("user");
+var password = document.getElementById("pd");
+
+var params = "lorem=ipsum&name=binny";
+http.open("POST", url, true);
+
+//Send the proper header information along with the request
+http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+http.onreadystatechange = function() {//Call a function when the state changes.
+    if(http.readyState == 4 && http.status == 200) {
+        alert(http.responseText);
+    }
+}
+http.send(params);
